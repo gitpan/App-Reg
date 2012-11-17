7b52b2e2de5919c4ffc54e4acf85156d1206af6d
Konrad Borowski <glitchmr@myopera.com>
2012-11-17 13:59:33 +0000

    Import of GLITCHMR/App-Reg-1.2.2 from CPAN.
    gitpan-cpan-distribution: App-Reg
    gitpan-cpan-version:      1.2.2
    gitpan-cpan-path:         GLITCHMR/App-Reg-1.2.2.tar.gz
    gitpan-cpan-author:       GLITCHMR
    gitpan-cpan-maturity:     released

--- lib/App/Reg.pm
+++ lib/App/Reg.pm
@@ -5,7 +5,7 @@ use Exporter 'import';
 # Import re, but without warnings about empty list
 use re ();
 
-our $VERSION = '1.2.0';
+our $VERSION = '1.2.2';
 our @EXPORT = our @EXPORT_OK = 'reg';
 
 sub reg {
